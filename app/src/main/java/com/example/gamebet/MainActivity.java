b849e5b85185227835c324d70668c77f9e1a5e36
vaturn <will6867@naver.com>
2024-07-06 00:48:43 +0900

    Add money and related functions

--- app/src/main/java/com/example/gamebet/MainActivity.java
+++ app/src/main/java/com/example/gamebet/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.gamebet;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gamebet.object.Money;
 
+public class MainActivity extends AppCompatActivity {
+    TextView tv_money;
+    Button btn_p, btn_m;
+    Money user_account = new Money();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        tv_money = findViewById(R.id.tv_money);
+        btn_m = findViewById(R.id.btn_m);
+        btn_p = findViewById(R.id.btn_p);
+
+        btn_p.setOnClickListener(view -> {
+            user_account.addMoney(10);
+
+            refreshTv();
+        });
+
+        btn_m.setOnClickListener(view -> {
+            user_account.withdrawMoney(10);
+
+            refreshTv();
+        });
+
+        user_account.setMoney(0);
+
+        refreshTv();
+        /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        });*/
+    }
+
+    public void refreshTv(){
+        String s = Integer.toString(user_account.getMoney());
+        tv_money.setText(s);
     }
 }
---
+++ app/src/main/java/com/example/gamebet/object/Money.java
@@ -0,0 +1,28 @@
+package com.example.gamebet.object;
+
+public class Money {
+    private int money;
+
+    public Money() {
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void addMoney(int money){
+        this.money += money;
+    }
+
+    public int withdrawMoney(int money){
+        if(this.money < money)
+            return -1;
+
+        this.money -= money;
+        return 0;
+    }
+}
